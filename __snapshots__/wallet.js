18619e19a758719da8017775a85f864e0934fbe1
ryanml <rlanese@asu.edu>
2018-05-15 20:45:59 -0700

    More stub work, avoid appActions, resetting state/stateFile

--- __snapshots__/wallet.js
+++ __snapshots__/wallet.js
@@ -22,3 +22,70 @@ exports['wallet create 1'] = {
   },
   "publishers": {}
 }
+
+exports['wallet recovery recovers 1'] = {
+  "cache": {
+    "ledgerVideos": {}
+  },
+  "ledger": {
+    "synopsis": {
+      "options": {
+        "_a": 7000,
+        "_b": 1000,
+        "scorekeeper": "concave",
+        "_d": 0.000033333333333333335,
+        "numFrames": 30,
+        "frameSize": 86400000,
+        "emptyScores": {
+          "concave": 0,
+          "visits": 0
+        },
+        "_b2": 1000000,
+        "scorekeepers": [
+          "concave",
+          "visits"
+        ],
+        "_a2": 14000,
+        "_a4": 28000,
+        "minPublisherVisits": 0,
+        "minPublisherDuration": 0
+      }
+    },
+    "about": {
+      "synopsisOptions": {
+        "_a": 7000,
+        "_b": 1000,
+        "scorekeeper": "concave",
+        "_d": 0.000033333333333333335,
+        "numFrames": 30,
+        "frameSize": 86400000,
+        "emptyScores": {
+          "concave": 0,
+          "visits": 0
+        },
+        "_b2": 1000000,
+        "scorekeepers": [
+          "concave",
+          "visits"
+        ],
+        "_a2": 14000,
+        "_a4": 28000,
+        "minPublisherVisits": 0,
+        "minPublisherDuration": 0
+      },
+      "synopsis": []
+    },
+    "info": {
+      "created": true,
+      "bravery": {
+        "fee": {
+          "currency": "USD",
+          "amount": 10
+        }
+      }
+    }
+  },
+  "about": {
+    "preferences": {}
+  }
+}
