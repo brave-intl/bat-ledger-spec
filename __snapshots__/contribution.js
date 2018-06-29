e51d6b904418bcb1729b031053b9435572a05b70
Jason Sadler <jsadler@brave.com>
2018-06-28 23:10:06 -0400

    Added contribution test harness
    Added contribution test harness
    
    Removed unused publisher method from wallet
    
    lint
    
    Reworked logic to break out of run loop for contribution test.
    
    Added fuzzing to main test.
    
    Modified contributions test to create wallet vice using pre-made example
    
    Updating contributions setup and teardown logic

---
+++ __snapshots__/contribution.js
@@ -0,0 +1,35 @@
+exports['contribution (contribution tests run for approximately 15 seconds) Tries to run contribution before time to reconcile 1'] = []
+
+exports['contribution (contribution tests run for approximately 15 seconds) Tries to run contribution at reconcile with enough funds and met browsing requirements 1'] = [
+  {
+    "viewingId": "2c61e750-a08f-4098-bcd4-5006525ebb25",
+    "contribution": {
+      "fiat": {
+        "amount": 10,
+        "currency": "BAT"
+      },
+      "rates": {
+        "EUR": 0.2190412601894385,
+        "USD": 0.25554448620000003,
+        "BCH": 0.00033478155925657733,
+        "BTC": 0.00004514,
+        "DASH": 0.00102477365436275,
+        "BTG": 0.009849390389672483,
+        "XRP": 0.5155155155155156,
+        "ETH": 0.0005365421680470907,
+        "LTC": 0.0029730119786404966
+      },
+      "altcurrency": "BAT",
+      "probi": "20000000000000000000"
+    },
+    "submissionStamp": 1530120720753,
+    "votes": 58,
+    "count": 58,
+    "ballots": {
+      "clifton.io": 13,
+      "bumpsmack.com": 26,
+      "brianbondy.com": 19
+    },
+    "names": {}
+  }
+]
---
+++ js/contribution.js
@@ -0,0 +1,170 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this file,
+* You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+'use strict'
+
+const clientHelper = require('../lib/helpers/clientHelper')
+const Contribution = require('../abs/contribution')
+const ledgerState = require('../browser-laptop/app/common/state/ledgerState')
+const ledgerStateData = require('../lib/data/ledger-state-funded.json')
+const ledgerStateOriginalData = require('../lib/data/ledger-state.json')
+const reconcileResponses = require('../lib/data/contribution-responses')
+const sinon = require('sinon')
+const now = new Date()
+const underscore = require('underscore')
+const Immutable = require('immutable')
+const msecs = {
+  minute: 1000 * 60
+}
+
+class JS extends Contribution {
+  constructor () {
+    super()
+    this.paymentEnabled = true
+    this.paymentsAllowNonVerified = true
+  }
+
+  runBefore (mockery) {
+    this.before(mockery)
+    process.env.LEDGER_ENVIRONMENT = 'staging'
+    process.env.LEDGER_NO_DELAY = true
+  }
+
+  beforeEach () {
+    this.paymentEnabled = true
+    this.paymentsAllowNonVerified = true
+    sinon.useFakeTimers(now.getTime())
+    this.setStateFile()
+    this.ledgerStatuses = require('../browser-laptop/app/common/constants/ledgerStatuses')
+    this.ledger = require('../browser-laptop/app/browser/api/ledger')
+    this.loadStubs(['contribution'])
+    this.ledger.setSynopsis(null)
+  }
+
+  after (mockery) {
+    mockery.deregisterAll()
+    mockery.disable()
+  }
+
+  afterEach (mockery) {
+    mockery.resetCache()
+    this.resetStateFile()
+    this.ledger.setClient(null)
+    this.state = this.defaultAppState
+    if (this.ledger.getBreakRun()) {
+      this.ledger.setBreakRun(false)
+    }
+    this.ledger = null
+  }
+
+  clientSetup (reconcileStamp) {
+    this.client = this.ledger.getClient()
+    this.client.state.reconcileStamp = reconcileStamp
+    this.client.credentials = {
+      persona: {
+        parameters: {
+          userId: '65ba48f6-591b-437c-ae74-219c8c5d4921'
+        }
+      }
+    }
+    const fCredential = reconcileResponses['reconcileCredentialFinalizeResponse']
+    const fPayload = reconcileResponses['reconcileCredentialSubmitResponse']
+    if (!this.client.credentialFinalize) {
+      this.client.credentialFinalize = sinon.stub(this.client, 'credentialFinalize').callsFake(function (credential, verification, callback) {
+        callback(null, fCredential)
+      })
+    }
+    this.client.credentialSubmit = sinon.stub(this.client, 'credentialSubmit').callsFake(function (ballots, callback) {
+      callback(null, fPayload)
+    })
+    this.ledger.setClient(this.client)
+  }
+
+  setStateFile () {
+    this.stateFile = JSON.stringify(ledgerStateData)
+  }
+
+  resetStateFile () {
+    this.stateFile = JSON.stringify(ledgerStateOriginalData)
+  }
+
+  addPublishersToLedger () {
+    this.addPublisherToLedger('https://clifton.io', 1)
+    this.addPublisherToLedger('https://brianbondy.com', 2)
+    this.addPublisherToLedger('https://bumpsmack.com', 3)
+  }
+
+  addPublisherToLedger (location, tabId = true) {
+    this.setState(this.ledger.addNewLocation(this.state, location, tabId, false, true))
+    this.setState(this.ledger.pageDataChanged(this.state, {}, true))
+  }
+
+  fuzzPublishers () {
+    let synopsis = this.ledger.getSynopsis()
+    underscore.keys(synopsis.publishers).forEach((publisher) => {
+      synopsis.publishers[publisher].duration = synopsis.publishers[publisher].window[0].duration = 15 * msecs.minute
+      synopsis.publishers[publisher].faviconURL = ''
+      synopsis.publishers[publisher].protocol = ''
+    })
+    this.ledger.setSynopsis(synopsis)
+    this.state = this.state.setIn(['ledger', 'synopsis', 'publishers'], Immutable.fromJS(synopsis.publishers))
+  }
+
+  initWallet () {
+    this.paymentEnabled = true
+    this.setState(clientHelper.clientInit(this.ledger, this.state))
+    this.ledger.setBreakRun(true)
+    this.setState(clientHelper.clientBoot(this.ledger, this.state, this.ledger.getClient()))
+    this.ledger.setBreakRun(false)
+  }
+
+  staticizeBallots () {
+    let transaction = this.state.getIn(['ledger', 'info', 'transactions']).toJS()
+    transaction[0].ballots['brianbondy.com'] = 19
+    transaction[0].ballots['bumpsmack.com'] = 26
+    transaction[0].ballots['clifton.io'] = 13
+    this.state = this.state.setIn(['ledger', 'info', 'transactions'], transaction)
+  }
+
+  createWallet () {
+    this.setState(this.ledger.enable(this.state))
+  }
+
+  contributionBeforeTime () {
+    this.initWallet()
+    this.clientSetup(1719633600000)
+    this.addPublishersToLedger()
+    this.fuzzPublishers()
+    this.state = ledgerState.setInfoProp(this.state, 'reconcileStamp', 1719633600000) // 06/29/2024
+    this.ledger.run(this.state, 0)
+    return ledgerState.getInfoProp(this.state, 'transactions') || []
+  }
+
+  contributionMinusFunds () {
+    this.paymentEnabled = true
+    return this.ledger.getSynopsis()
+  }
+
+  contributionAdequateFundsReqMet () {
+    this.initWallet()
+    this.clientSetup(1528948800000)
+    this.addPublishersToLedger()
+    this.fuzzPublishers()
+    this.state = ledgerState.setInfoProp(this.state, 'reconcileStamp', 1528948800000)
+    this.ledger.run(this.state, 0)
+    this.staticizeBallots()
+    return ledgerState.getInfoProp(this.state, 'transactions') || []
+  }
+
+  contributionAdequateFundsReqNotMet () {
+    this.initWallet()
+    this.clientSetup(1528948800000)
+    this.addPublishersToLedger()
+    this.state = ledgerState.setInfoProp(this.state, 'reconcileStamp', 1528948800000)
+    this.ledger.run(this.state, 0)
+    return ledgerState.getInfoProp(this.state, 'transactions') || []
+  }
+}
+
+module.exports = JS
